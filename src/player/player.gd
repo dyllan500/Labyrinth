6d885da820117ccb48d4e885fb80cdd761d71b96
Dyllan <ddk5154@psu.edu>
2024-10-27 13:32:50 -0400

    changed user input to move per keypress

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,9 +1,17 @@
 class_name Player extends CharacterBody2D
 
-@export var TILE_SIZE = 8
+@export var TILE_SIZE = 16
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
+	var input_direction = Vector2(0,0)
+	if Input.is_action_just_pressed("right"):
+		input_direction = Vector2(1,0)
+	elif Input.is_action_just_pressed("left"):
+		input_direction = Vector2(-1,0)
+	elif Input.is_action_just_pressed("up"):
+		input_direction = Vector2(0,-1)
+	elif Input.is_action_just_pressed("down"):
+		input_direction = Vector2(0,1)
 	position += input_direction * TILE_SIZE
 
 func _physics_process(delta):
